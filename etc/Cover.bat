fc70b5a075718f093eeb5ad41ee756073d473d83
Pancho <francisco.rosales@gmail.com>
2017-09-06 23:12:18 -0300

    Updated Readme.me, added OpenCover

--- etc/Cover.bat
+++ etc/Cover.bat
@@ -1,3 +1,3 @@
-OpenCover.Console.exe -register:user -target:"..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" -targetargs:"..\src\FdvRentalBike.UnitTest\bin\Debug\FdvRentalBike.UnitTest.dll" -filter:"+[FdvRentalBike.Workflow*]* -[*]*Resource"
+..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" -targetargs:"..\src\FdvRentalBike.UnitTest\bin\Debug\FdvRentalBike.UnitTest.dll" -filter:"+[FdvRentalBike.Workflow*]* -[*]*Resource"
 
 ..\packages\ReportGenerator.2.5.11\tools\ReportGenerator.exe -reports:results.xml -targetdir:GeneratedReports
